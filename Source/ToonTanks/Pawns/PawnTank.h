290a5950b1f0dede28bdc9761d84ffac80314397
Ali <ali.valizadeh.h@gmail.com>
2020-09-15 17:20:23 +0200

    Adding PawnTank Setting up the camera Setting up the movement

---
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawnBase.h"
+#include "PawnTank.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API APawnTank : public APawnBase
+{
+    GENERATED_BODY()
+
+public:
+    APawnTank();
+
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+
+private:
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera", meta = (AllowPrivateAccess="true"))
+    USpringArmComponent* SpringArmComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera", meta = (AllowPrivateAccess="true"))
+    UCameraComponent* CameraComponent;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement", meta = (AllowPrivateAccess="true"))
+    float MoveSpeed = 100.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement", meta = (AllowPrivateAccess="true"))
+    float RotateSpeed = 100.0f;
+
+    FVector MoveDirection;
+    FQuat RotationDirection;
+
+    void CalculateMoveInput(float Value);
+    void CalculateRotateInput(float Value);
+
+    void Move();
+    void Rotate();
+};
