a37e39b08735e8169014a5ba5ae04b0e45ad9f85
Ali <ali.valizadeh.h@gmail.com>
2020-09-22 00:44:35 +0200

    Add health component to Tank and Turret

---
+++ Source/ToonTanks/Components/HealthComponent.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ToonTanks/GameModes/TankGameModeBase.h"
+#include "UObject/ObjectMacros.h"
+
+#include "HealthComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TOONTANKS_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void TakeDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType,
+		AController* InstigatedBy, AActor* DamageCauser);
+	
+private:
+
+	UPROPERTY(EditAnywhere, Category="Health", meta=(AllowPrivateAccess="true"))
+	float DefaultHealth = 100.f;
+
+	float Health = 0.f;
+
+	ATankGameModeBase* GameModeRef;
+};
